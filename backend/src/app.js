99f0dcd8048ad8a4406e3d640d84a855b598cfe6
EagleEyeRaider <yatindanger0001@gmail.com>
2025-01-19 12:56:10 +0530

    include express rate limiter and bug fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,9 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import session from "express-session";
 import passport from "passport";
+import { rateLimit } from "express-rate-limit";
+import requestIp from "request-ip";
+import { ApiError } from "./utils/ApiError.js";
 
 
 const app = express()
@@ -11,6 +14,26 @@ app.use(cors({
   origin: process.env.CORS_ORIGIN,  
   credentials: true
 }))
+app.use(requestIp.mw());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req, res) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+app.use(limiter);
 app.use(express.json({ limit: "32kb" }))
 app.use(express.urlencoded({ extended: true, limit: "32kb" }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
